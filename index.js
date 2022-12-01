1b42c921444909b97ba5248fb9b68bb0722c22cb
caroliyw <caroliyw@uci.edu>
2022-12-01 09:34:17 -0800

    implemented shaders!

--- index.js
+++ index.js
@@ -1,10 +1,146 @@
 //
 // start here
 //
-function main() {
-	const canvas = document.querySelector("#glCanvas");
+
+var gl;
+var canvas;
+var shaderProgram;
+
+var vertexPositionBuffer;
+var vertexColorBuffer;
+
+
+function createShaders() {
+	let vert_shade = 
+		'attribute vec3 vertcoordinates;' + 
+		'attribute vec4 verColor;' + 
+		'varying vec4 vColor;' + 
+		'void main()' +
+		'{' + 
+			'gl_Position = vec4(vertcoordinates, 1.0);' + 
+			'vColor = verColor;' + 
+		'}';
+
+	let frag_shade = 
+		'precision mediump float;' + 
+		'varying vec4 vColor;' + 
+		'void main()'+
+		'{' +
+			'gl_FragColor = vColor;' +
+		'}'; 
+
+	var vShader = gl.createShader(gl.VERTEX_SHADER );
+	gl.shaderSource(vShader, vert_shade);
+	gl.compileShader(vShader);
+
+	if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vShader));
+		return;
+	}
+
+	var fShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fShader, frag_shade);
+	gl.compileShader(fShader);
+
+	if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fShader));
+		return;
+	}
+
+	shaderProgram = gl.createProgram(); 
+	gl.attachShader(shaderProgram, vShader);
+	gl.attachShader(shaderProgram, fShader);
+	gl.linkProgram(shaderProgram);
+
+
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		console.error('ERROR linking program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+	gl.validateProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+
+	gl.useProgram(shaderProgram);
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertcoordinates");
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+  
+	shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "verColor");
+	gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+}
+
+function createBuffers() {
+	vertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+
+	// gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+
+	var verts = [
+		0.0, 0.7, 0.0,
+		-0.7, -0.7, 0.0,
+		0.7, -0.7, 0.0,
+
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+	vertexPositionBuffer.itemSize = 3;
+	vertexPositionBuffer.numberOfItems = 3;
+
+
+
+	vertexColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+
+	var colors = [
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+	];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	vertexColorBuffer.itemSize = 4;
+	vertexColorBuffer.numItems = 3;
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
+						   vertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
+							  vertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+	gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+
+
+}
+
+function createGLContext(canvas) {
+	var names = ["webgl", "experimental-webgl"];
+	var context = null;
+	for (var i=0; i < names.length; i++) {
+	  try {
+		context = canvas.getContext(names[i]);
+	  } catch(e) {}
+	  if (context) {
+		break;
+	  }
+	}
+	if (context) {
+	  context.viewportWidth = canvas.width;
+	  context.viewportHeight = canvas.height;
+	} else {
+	  alert("Failed to create WebGL context!");
+	}
+	return context;
+  }
+
+
+
+function initGL() {
+	canvas = document.querySelector("#glCanvas");
 	// Initialize the GL context
-	const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl"); 
+	gl = createGLContext(canvas);
   
 	// Only continue if WebGL is available and working
 	if (gl === null) {
@@ -13,11 +149,18 @@ function main() {
 	  );
 	  return;
 	}
-  
-	// Set clear color to black, fully opaque
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	// Clear the color buffer with specified clear color
 	gl.clear(gl.COLOR_BUFFER_BIT);
+	
+
+	createShaders();
+	createBuffers();
+
+	gl.enable(gl.DEPTH_TEST);
+  
+	// Set clear color to black, fully opaque
+
   }
   
 //   window.onload = main;
