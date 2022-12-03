35f6feab9c6e915b3d9c40771dbee844552a2eb5
Matt Gies <matt.bgies@gmail.com>
2022-12-03 11:55:19 -0800

    split into many functions, made shaders into multiline strs for easier editing, updated mvMatrix, implemented basic diffuse shader

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-//
-// start here
-//
-
-
-
-// const OBJ = require("./js_files/webgl-obj-loader");
-
 var gl;
 var canvas;
 var shaderProgram;
@@ -13,47 +5,55 @@ var shaderProgram;
 var vertexPositionBuffer;
 var vertexColorBuffer;
 
+var objsToDraw; // collection of OBJ.Mesh objects with initialized buffers which can be used to draw
 var mesh;
 var mvMatrix; // model-view matrix
 var nMatrix; // normal matrix
 var projMatrix; // projection matrix for MVP transformations
-var lightPos;
+var lightPos; // position of the light, used as a uniform for shader calculations
 
 
 function createShaders() { // modify parameters if make multiple shaders
-	let vert_shade = 
-		'uniform mat4 uMVMatrix;' +
-		'uniform mat4 uNMatrix;' +
-		'uniform mat4 uProjMatrix;' +
-		'attribute vec3 vCoords;' + 
-		'attribute vec3 aVertNorm;' +
-		
-		'varying vec4 vColor;' + 
-		'varying vec3 vPosition;' + // vertex position in cam space
-		'varying vec3 vNorm;' +
-		
-		'void main()' +
-		'{' + 
-			'vec4 camSpacePos = uMVMatrix * vec4(vCoords, 1.0) ;' +
-			'vPosition = vec3(camSpacePos);' +
-			'gl_Position = uProjMatrix * camSpacePos;' +
-			'vColor = vec4((aVertNorm), 1.0);' +
+	let vert_shade = `
+		uniform mat4 u_mvMatrix;
+		uniform mat4 u_nMatrix;
+		uniform mat4 u_pMatrix;
 
-			// TODO add normal matrix to transform normals to camera space
-			'vNorm = vec3(uNMatrix * vec4(aVertNorm, 1.0));' +
-		'}';
+		attribute vec3 a_vCoords;
+		attribute vec3 a_vNorm;
 
-	let frag_shade = 
-		'precision mediump float;' + 
-		'uniform vec3 uLightPos;' +
-		'varying vec4 vColor;' +
-		'varying vec3 vPosition;' +
-		'varying vec3 vNorm;' +
-		'void main()' +
-		'{' +
-			// 'gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);' +
-			'gl_FragColor = vec4(dot(normalize(uLightPos - vPosition), vNorm) * vNorm, 1.0);' +
-		'}'; 
+		varying vec4 v_vColor;
+		varying vec3 v_vPos;
+		varying vec3 v_vNorm;
+
+		void main() {
+			vec4 camSpacePos = u_mvMatrix * vec4(a_vCoords, 1.0) ;
+			v_vPos = vec3(camSpacePos);
+			gl_Position = u_pMatrix * camSpacePos;
+			v_vColor = vec4((a_vNorm), 1.0);
+			v_vNorm = vec3(u_nMatrix * vec4(a_vNorm, 1.0));
+		}
+	`;
+
+	let frag_shade = `
+		precision mediump float;
+
+		uniform vec3 u_lightPos;
+
+		varying vec4 v_vColor;
+		varying vec3 v_vPos;
+		varying vec3 v_vNorm;
+
+		void main() {
+			// basic diffuse shader implementation
+			vec3 Kd = vec3(0.15, 0.95, 0.6);
+			float I = 5.0;
+			float maxDot = max(0.0, dot(v_vNorm, u_lightPos - v_vPos));
+			float rSquared = length( u_lightPos - v_vPos ) * length( u_lightPos - v_vPos );
+
+			gl_FragColor = vec4((I / rSquared * maxDot * Kd), 1.0);
+		}
+	`; 
 
 	// create and compile vertex shader
 	var vShader = gl.createShader(gl.VERTEX_SHADER);
@@ -119,80 +119,82 @@ function createGLContext(canvas) {
   }
 
 
-function initObj(obj_string) {
-	mesh = new OBJ.Mesh(obj_string);
-	OBJ.initMeshBuffers(gl, mesh);
-}
+function setUpMVMatrix() {
+	// uses inverse of the camera's transformations to set up the mvMatrix
+	// that way, all objects get transformed into camera space with the mvMatrix
+	// camera space = (cam at (0, 0, 0) facing down -Z axis)
+	let camTransforms = mat4.create();
+	mat4.identity(camTransforms);
+	mat4.rotateX(camTransforms, Math.PI / 6);
+	mat4.rotateY(camTransforms, rotAmount);
+	mat4.translate(camTransforms, [0.0, 0.2, 6.0]);
 
-
-function setUpMatricesPoolSides() {
-	// implement transforms
-	// mvMatrix setup
-	mvMatrix = mat4.create();
-
-	mat4.identity(mvMatrix);
-	mat4.translate( mvMatrix, [0.0, 0.0, -6.0 ] );
-	mat4.rotateY( mvMatrix, rotAmount );
-	mat4.rotateX( mvMatrix, Math.PI / 3);
-	mat4.rotateY( mvMatrix, - 3 * Math.PI / 4);
+	mvMatrix = mat4.inverse(camTransforms);
 	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
 
 
-function setUpMatricesPoolSurface() {
-	// implement transforms
-	// mvMatrix setup
-	mvMatrix = mat4.create();
+function setUpProjMatrix() {
+	projMatrix = mat4.create();
+	mat4.identity(projMatrix);
+	mat4.perspective(36, gl.viewportWidth / gl.viewportHeight, 0.1, 1000, projMatrix);
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, projMatrix);
+}
 
-	mat4.identity(mvMatrix);
-	mat4.translate( mvMatrix, [0.0, 0.0, -6.0] );
-	mat4.rotateY( mvMatrix, rotAmount );
-	mat4.rotateX( mvMatrix, Math.PI / 3);
-	mat4.rotateY( mvMatrix, - 3 * Math.PI / 4);
-	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+
+function setUpNMatrix() {
+	nMatrix = mat4.inverse(mvMatrix);
+	mat4.transpose(nMatrix);
+	gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
+}
+
+
+function setUpLightPos() {
+	lightPos = vec3.create([0.0, 5.0, 0.0]);
+	gl.uniform3fv(shaderProgram.lightPosUniform, lightPos);
 }
 
 
 function setUpShaderAttribs() {
-	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vCoords");
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "a_vCoords");
 	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-	shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertNorm");
+	shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "a_vNorm");
 	gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
 }
 
+
 function setUpShaderUniforms() {
-	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uProjMatrix");
-	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
-	shaderProgram.lightPosUniform = gl.getUniformLocation(shaderProgram, "uLightPos");
+	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "u_mvMatrix");
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "u_pMatrix");
+	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "u_nMatrix");
+	shaderProgram.lightPosUniform = gl.getUniformLocation(shaderProgram, "u_lightPos");
 
-	nMatrix = mat4.inverse(mvMatrix);
-	mat4.transpose(nMatrix);
-	gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
-
-	lightPos = vec3.create([0.0, 15.0, -6.0]);
-	gl.uniform3fv(shaderProgram.lightPosUniform, lightPos);
-
-	projMatrix = mat4.create();
-	mat4.identity(projMatrix);
-	mat4.perspective(36, gl.viewportWidth / gl.viewportHeight, 0.1, 1000, projMatrix);
-	// console.log(projMatrix);
-	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, projMatrix);
+	setUpMVMatrix();
+	setUpProjMatrix();
+	setUpNMatrix();
+	setUpLightPos();
 }
+
 
 function drawScene() {
-	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	gl.clearColor(clearColorR, clearColorG, clearColorB, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	for (obj of objsToDraw) {
+		mesh = obj;
+		gl.bindBuffer(gl.ARRAY_BUFFER, mesh.normalBuffer);
+		gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mesh.normalBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
-    gl.drawElements(gl.TRIANGLES, mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, mesh.vertexBuffer);
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mesh.vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer);
+		gl.drawElements(gl.TRIANGLES, mesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+	}
 }
 
-var clearColorR = 0.2;
-// console.log(clearColorR);
+
+var clearColorR = 0.35;
 var clearColorG = 0.5;
 var clearColorB = 0.7;
 var lastTime = 0;
@@ -208,25 +210,29 @@ function tick() {
 	}
 	lastTime = timeNow;
 
-	gl.clearColor(clearColorR, clearColorG, clearColorB, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	// the only uniforms that update on each tick are the modelview and normal matrices
+	// the projection matrix and light pos vector stay the same every tick
+	setUpMVMatrix();
+	setUpNMatrix();
 
-	initObj(pool_sides_and_bottom);
-	setUpMatricesPoolSides();
-	setUpShaderAttribs();
-	setUpShaderUniforms();
 	drawScene();
-
-	// initObj(one_plane);
-	// setUpMatricesPoolSurface();
-	// setUpShaderAttribs();
-	// setUpShaderUniforms();4
-	// drawScene();
 }
 
+
+function getObjectsToDraw(obj_str_names) {
+	objsToDraw = [];
+	for (obj_name of obj_str_names) {
+		mesh = new OBJ.Mesh(obj_name);
+		OBJ.initMeshBuffers(gl, mesh);
+		objsToDraw.push(mesh);
+	}
+}
+
+
 function initGL() {
-	canvas = document.querySelector("#glCanvas");
+	// initGL is called on page load
 	// Initialize the GL context
+	canvas = document.querySelector("#glCanvas");
 	gl = createGLContext(canvas);
   
 	// Only continue if WebGL is available and working
@@ -237,10 +243,16 @@ function initGL() {
 	  return;
 	}
 
-	createShaders();
-
 	gl.enable(gl.DEPTH_TEST);
-	initObj(bunny_mesh_str);
+
+	createShaders();
+	getObjectsToDraw([
+		pool_sides_and_bottom,
+		one_plane
+	]);
+
+	setUpShaderAttribs();
+	setUpShaderUniforms();
+
 	tick();
-  
   }
