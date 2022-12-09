5675002eed5d9d7471a373d3341efbcfe98459a3
Matthew Gies <matt.bgies@gmail.com>
2022-12-09 14:56:16 -0800

    simplified shader code by removing commented out lines + getting rid of unused nMatrix uniform

--- shader_strs/water_frag_shader.js
+++ shader_strs/water_frag_shader.js
@@ -3,13 +3,9 @@ var water_frag_shader = `
 		precision mediump float;
 
 		uniform vec3 u_lightPos; // light position in world-space coordinates ( needs MV matrix to be converted to cam space)
-		uniform mat4 u_mvMatrix;
-		uniform mat4 u_nMatrix;
 		uniform sampler2D u_waterNormalMap;
-		uniform float u_totalTimeElapsed;
 		uniform float u_lightIntensity;
 
-		varying vec3 v_vPos; // camera-space coordinates for position of the current fragment
 		varying vec3 v_vPosWorldSpace;
 		varying vec2 v_vTexCoords1; // (u,v) tex coords which have already been adjusted with rotation and time offset
 		varying vec2 v_vTexCoords2;
@@ -23,11 +19,8 @@ var water_frag_shader = `
 
 			vec3 worldSpaceNormal = normalize(wSN1CorrectDirs + wSN2CorrectDirs);
 
-			
 			vec3 vi = u_lightPos - v_vPosWorldSpace;
 			float normalized_n_i_dot = dot(worldSpaceNormal, normalize(vi));
-			// gl_FragColor = vec4(normalized_n_i_dot);
-			// return;
 			float uAmbient = 0.1; // line 141
 			vec3 uSpecularColor = vec3(1.0, 1.0, 1.0); // line 140
 			vec3 uDiffuseColor = vec3(0.2392, 0.5216, 0.7765); // line 139 in pa2_webgl.js
@@ -35,14 +28,12 @@ var water_frag_shader = `
 			float uLightPower = u_lightIntensity * 7.0;
 			float uExponent = 5.0;
 
-
 			if (normalized_n_i_dot > 0.0) {
 				vec3 normalized_o = - v_vPosWorldSpace / length(v_vPosWorldSpace);
 				vec3 normalized_n = worldSpaceNormal; // already normalized
 				vec3 normalized_i = normalize(vi);
 				vec3 normalized_h = normalize(normalized_i + normalized_o);
 
-				
 				float vI = uLightPower / (pow(length(vi),2.0) / 5.0 + 5.0);
 				float inner_left_term = vI * normalized_n_i_dot + uAmbient;
 				vec3 left_term = inner_left_term * uDiffuseColor;
